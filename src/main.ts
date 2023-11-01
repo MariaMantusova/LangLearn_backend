94ee68aea250f76924fea9607252905f4b1d57c5
MariaMantusova <maria.mantusova@mail.ru>
2023-11-01 16:49:28 +0300

    feat: add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
+import { NestFactory } from "@nestjs/core";
+import * as csurf from "csurf";
+import * as cookieParser from "cookie-parser";
+import helmet from "helmet";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(helmet());
   app.use(cookieParser());
+  app.use(csurf());
 
   await app.listen(3000);
 }
+
 bootstrap();
