d940b99d247c44292d4f6aac4032a02afe0c878b
MariaMantusova <maria.mantusova@mail.ru>
2023-11-02 16:27:32 +0300

    feat: add cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import helmet from "helmet";
 import { AppModule } from "./app.module";
+const cookieParser = require('cookie-parser')
 
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(cookieParser())
   app.use(helmet());
 
   await app.listen(3000);
