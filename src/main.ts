85d68f1d2be75c9d176fb9d7a15cd15cc3dc9883
MariaMantusova <maria.mantusova@mail.ru>
2023-11-02 12:22:43 +0300

    feat: change port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import * as cookieParser from "cookie-parser";
 import helmet from "helmet";
 import { AppModule } from "./app.module";
 
-async function bootstrap() {
+export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
