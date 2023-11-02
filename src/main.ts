ecdbc41e66f4acf32b61ff0666c81f04724f542e
MariaMantusova <maria.mantusova@mail.ru>
2023-11-02 16:05:10 +0300

    feat: delete cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from "@nestjs/core";
-import * as cookieParser from "cookie-parser";
 import helmet from "helmet";
 import { AppModule } from "./app.module";
 
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cookieParser());
   app.use(helmet());
 
   await app.listen(3000);
