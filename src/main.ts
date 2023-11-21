a9283be6464f51f5308f06484b4a7f4c41de2421
MariaMantusova <maria.mantusova@mail.ru>
2023-11-21 17:44:52 +0300

    feat: add cors use

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,11 @@ const cookieParser = require('cookie-parser')
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
   });
   app.use(cookieParser())
   app.use(helmet());
